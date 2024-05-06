e67c8be48d46d9529191f72e3bc37c3d88636082
Tota-Ota <anirudhmalhotra30@gmail.com>
2024-05-06 22:08:30 +0530

    Update model_train.py
    Added the prediction to predict the values based on the given input data of sensors

--- model/model_train.py
+++ model/model_train.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error
 
-data = pd.read_csv("dataset\data_cleaned.csv")
+df = pd.read_csv('dataset\data_cleaned.csv')
 
-X = data.drop(columns=['Potability'])
-y = data['Potability']
+X = df[['ph', 'Turbidity']]
+y = df.drop(columns=['ph', 'Turbidity'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -14,12 +14,9 @@ model = LinearRegression()
 
 model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+predictions = model.predict(X_test)
 
-mae = mean_absolute_error(y_test, y_pred)
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-print("Mean Absolute Error:", mae)
-print("Mean Squared Error:", mse)
-print("R-squared Score:", r2)
+#Example
+input_values = [[7.0, 5.6]]
+output_values = model.predict(input_values)
+print("Predicted Water Quality Parameters:", output_values)
