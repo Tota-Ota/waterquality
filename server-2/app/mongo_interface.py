3fcd1741798006eb3f5ebda7e4b0a4cf6fb47cef
Karan Anand <thekarananand@gmail.com>
2024-06-07 17:59:25 +0530

    fix: minor-fix

--- server-2/app/mongo_interface.py
+++ server-2/app/mongo_interface.py
@@ -2,6 +2,7 @@ import pymongo
 
 # Create/Access MongoClient to connect to Mongo Container
 mongo_container = pymongo.MongoClient( 'mongo' , 27017 )
+# mongo_container = pymongo.MongoClient( 'localhost' , 80 )
 
 # Create/Access DB named minor-project
 db_minor_project = mongo_container["minor-project"]
